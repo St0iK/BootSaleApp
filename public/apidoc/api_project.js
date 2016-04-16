652badfccaa3484b560dfc501647cf5298f17832
Dimitris Stoikidis <jstoikidis@gmail.com>
2016-04-16 01:58:52 +0100

    Moves apidoc to correct forlder

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Project-A",  "version": "0.1.0",  "description": "API documentation for project-A",  "title": "Project-A",  "url": "project-alanis.herokuapp.com/api/v1",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-04-16T00:57:37.398Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
