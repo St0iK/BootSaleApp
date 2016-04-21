f8a879f550e6743ffbdff79f49ee0d4c05344063
Dimitris Stoikidis <jstoikidis@gmail.com>
2016-04-22 00:40:19 +0100

    Adds model Factory for Ads

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,8 +16,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'first_name' => $faker->name,
 		'last_name' => $faker->lastName,
 		'email' => $faker->email,
-		'username' => $faker->word,
+		'username' => $faker->word . str_random(10),
 		'password' => bcrypt($faker->password),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Ad::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => 1,
+		'title' => $faker->sentence,
+		'description' => $faker->sentence,
+		'category_id' => $faker->randomDigit, // set a random category id
+		'price' => $faker->randomFloat,
+        'latitude' => $faker->randomDigit,
+        'longitude' => $faker->randomDigit,
+        'currency_code' => $faker->currencyCode,
     ];
 });
