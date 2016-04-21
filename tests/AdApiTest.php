20c113b9d18fc52e975e43e6c7c595bb78d31d90
Dimitris Stoikidis <jstoikidis@gmail.com>
2016-04-22 00:41:01 +0100

    Updated documentation files

--- tests/AdApiTest.php
+++ tests/AdApiTest.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use Illuminate\Http\UploadedFile;
+use App\Ad;
+
 /**
  * TODO:
  *
@@ -18,7 +21,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class AdApiTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Test that GET for ads works
      *
      * @return void
      */
@@ -32,5 +35,109 @@ class AdApiTest extends TestCase
                          'status' => true,
                          'status_code' => 200
                      ]);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAdsGetValidationFailed()
+    {
+        $this->json('GET', 
+            '/api/v1/ads')->seeJson([
+                         'status' => false,
+                         "errors" => "Wrong request. Please include all required data"
+                     ]);
+                     
+    }
+
+    /**
+     * Basic test for creating new AD
+     *
+     * @return void
+     */
+    public function testCreateNewAd()
+    {
+        $ad = factory(App\Ad::class)->make();
+        $this->json('POST', '/api/v1/ads',$ad->toArray())
+                     ->seeJson([
+                         'status' => true,
+                         'status_code' => 201
+                     ]);
+                     
+    }
+
+    /**
+     * Test ad creation with file upload to AWS
+     *
+     * @return void
+     */
+    public function testCreateNewAdWithPhotoUpload()
+    {
+        $file = $this->createLocalImageForTesting();
+        $ad = factory(App\Ad::class)->make();
+        $this->call('POST', '/api/v1/ads', $ad->toArray(), [], ['image_1' => $file], ['Accept' => 'application/json']);
+        $file = $this->testRemoteImage();
+       
+    }
+
+    /**
+     * Test Multile files upload
+     */
+    public function testCreateNewAdWithMultiplePhotoUpload()
+    {
+        $ad = factory(App\Ad::class)->make();
+        $file = $this->createLocalImageForTesting();
+        $this->call('POST', '/api/v1/ads', $ad->toArray(), [], ['image_1' => $file,'image_2' => $file,'image_3' => $file,'image_4' => $file,'image_5' => $file], ['Accept' => 'application/json']);
+        $this->testRemoteImage();
+        
+    }
+
+    /**
+     * Test validation for required fields
+     */
+    public function testAdPostValidationFail(){
+        
+        $post = ['category_id' => 6];
+
+        $this->json('POST', '/api/v1/ads', $post)
+                     ->seeJson([
+                         'status' => false,
+                         'message' => 'Please check your post, required fields are missing.'
+                     ]);
+    }
+
+    function createLocalImageForTesting()
+    {
+        $stub = __DIR__.'/stubs/test.png';
+        $name = str_random(8).'.png';
+        $path = sys_get_temp_dir().'/'.$name;
+        copy($stub, $path);
+        return new UploadedFile($path, $name, filesize($path), 'image/png', null, true);
+    }
+
+    function testRemoteImage()
+    {
+       $s3 = AWS::createClient('s3');
+       $lastInsertedAd = Ad::orderBy('id', 'desc')->with(['photos'])->first();
+       if(empty($lastInsertedAd->photos))
+       {
+           $this->assertTrue(FALSE, 'Nope it didnt work');
+       }
+
+       foreach ($lastInsertedAd->photos as $key => $photo) 
+       {
+           $keyname = $lastInsertedAd->photos[$key]['path'];
+           $result = $s3->getObject(array(
+               'Bucket' => \Config::get('aws.bucket'),
+               'Key'    => $keyname
+           ));
+           
+           if ($result)
+           {
+               $this->assertTrue(TRUE,'Worked');
+           }    
+       }
     }
 }
